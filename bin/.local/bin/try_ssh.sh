54f9558dd4a8965797bcb3b8d32f82418f31920e
Mike Hall <mike.hall@integrisit.com>
2025-10-30 10:11:08 -0500

    Add try_ssh script

---
+++ bin/.local/bin/try_ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ssh_retry() {
+    local host=$1 wait=1 max=60
+    until ssh "$host"; do
+        echo "Retry in ${wait}s..."
+        sleep $wait
+        ((wait = wait < max ? wait * 2 : max))
+    done
+}
